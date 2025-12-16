9e2b0df95fea26d84aa7ac6514da0555cf884223
Gabriele Corti <dernier.grie@gmail.com>
2025-12-16 15:43:56 +0100

    update dependencies

--- src/Dependencies.lua
+++ src/Dependencies.lua
@@ -2,6 +2,9 @@ require "src.globals"
 
 require "src.Player"
 require "src.Terrain"
+require "src.Score"
+require "src.Box"
+require "src.UI"
 
 require "src.StateStack"
 require "src.states.BaseState"
