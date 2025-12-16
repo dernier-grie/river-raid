d166bf780c93547f54ef7ad5197e4b768a67484a
Gabriele Corti <dernier.grie@gmail.com>
2025-12-16 09:02:33 +0100

    implement state stack

--- main.lua
+++ main.lua
@@ -1,48 +1,28 @@
-require "src.globals"
+require "src.Dependencies"
 
-require "src.Player"
-require "src.Terrain"
-
-local player = Player:new(WIDTH / 2, HEIGHT / 2)
-local terrain = Terrain:new()
-
-local shouldCrash = terrain:intersects(player)
 function love.load()
     love.window.setTitle("River raid")
     love.window.setMode(SCREEN_WIDTH, SCREEN_HEIGHT)
     love.graphics.setBackgroundColor(0.160, 0.678, 1)
-
-    love.keyboard.keypressed = {}
     love.graphics.setLineWidth(1)
+
+    GStateStack = StateStack:new(
+        {
+            PlayState:new()
+        }
+    )
+    love.keyboard.keypressed = {}
 end
 
 function love.update(dt)
-    player:update(dt)
-    if player.dx ~= 0 then
-        local width = player.width -
-            (player["banking"]) * math.abs(player["quads_index"] - player["quads_index_start"]) /
-            player["quads_index_gap"]
-        shouldCrash = terrain:intersects({
-            x = player.x - width / 2,
-            y = player.y,
-            width = width,
-            height = player.height,
-        })
-    end
-
+    GStateStack:update(dt)
 
     love.keyboard.keypressed = {}
 end
 
 function love.draw()
     love.graphics.scale(SCREEN_SCALE)
-
-    terrain:draw()
-    player:draw()
-
-    if shouldCrash then
-        love.graphics.print("Crash!")
-    end
+    GStateStack:draw()
 end
 
 function love.keyboard.waspressed(key)
