90ddcd27fe1809e33cfea5f05c4a98af38514e9e
Gabriele Corti <dernier.grie@gmail.com>
2025-12-16 15:41:13 +0100

    box table
    draw corner sprites at the ends, line above and below

---
+++ src/Box.lua
@@ -0,0 +1,22 @@
+Box = {}
+
+
+function Box:new(x, y, width, height)
+    local this = {
+        ["x1"] = x,
+        ["y1"] = y,
+        ["x2"] = x + width,
+        ["y2"] = y + height,
+    }
+
+    self.__index = self
+    setmetatable(this, self)
+    return this
+end
+
+function Box:draw()
+    love.graphics.line(self.x1, self.y1 + 0.5, self.x2, self.y1 + 0.5)
+    love.graphics.line(self.x1, self.y2 - 0.5, self.x2, self.y2 - 0.5)
+    love.graphics.draw(Texture, Quads.UI.boxStart, self.x1 - 1, self.y1)
+    love.graphics.draw(Texture, Quads.UI.boxEnd, self.x2, self.y1)
+end
