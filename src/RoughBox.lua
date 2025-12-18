6be68b3a909f35a856358fd20be9fd2f2231ca5b
Gabriele Corti <dernier.grie@gmail.com>
2025-12-18 15:18:22 +0100

    rough box
    alternative container

---
+++ src/RoughBox.lua
@@ -0,0 +1,21 @@
+RoughBox = {}
+
+local INSET_BOTTOM_RIGHT = 2
+
+function RoughBox:new(x, y, width, height)
+    local this = {
+        ["x"] = x,
+        ["y"] = y,
+        ["width"] = width,
+        ["height"] = height,
+    }
+
+    self.__index = self
+    setmetatable(this, self)
+    return this
+end
+
+function RoughBox:draw()
+    love.graphics.rectangle("fill", self.x, self.y, self.width, self.height - INSET_BOTTOM_RIGHT)
+    love.graphics.rectangle("fill", self.x, self.y, self.width - INSET_BOTTOM_RIGHT, self.height)
+end
